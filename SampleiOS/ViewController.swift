18039e15b7874739631b3d808d564237e25bc7b7
a503-26 <1>
2018-11-21 10:16:10 +0900

    Initial Commit

---
+++ SampleiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleiOS
+//
+//  Created by 503-26 on 21/11/2018.
+//  Copyright © 2018 person. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
